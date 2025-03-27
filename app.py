fe035afacf08320bcc5c87616f060536eefc6014
Aarash Feizi <aarash95@gmail.com>
2025-03-26 20:56:24 -0400

    warn better

--- app.py
+++ app.py
@@ -155,7 +155,7 @@ if not is_last_sample:
     with layout[0]:
         # variant_string = '**is**' if sample['var'] == 'variant' else 'is **NOT**'
         variant_string = '**DECREASES**' if sample['var'] == 'variant' else '**DOES NOT decrease**'
-        function = st.warning if sample['var'] == 'variant' else st.info
+        function = st.error if sample['var'] == 'variant' else st.success
         # second_part_of_string = "**Decrease score if they do not completely match.**" if sample['var'] == 'variant' else '**Ignore differences for final score.**'
         template = f"⚡️*Similarity conditions for **this** pair:* \n - *{split2humanreadable[sample['split']]} {variant_string} the score.* \n\n *If the images are different, decrease the score regardless.*"
         # st.markdown(f"{template.format(var=sample['var'].upper(), split=sample['split'])}")
